fb9a07cc9b8c00a8874f433b50bfc115f580af5b
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-10-02 17:34:45 +0500

    process.argv

---
+++ input.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const action= process.argv[2];
+const fileName= process.argv[3];
+const data= process.argv[4];
+
+if(action == 'create'){
+fs.writeFileSync(fileName, data)
+}else if(action == 'delete'){
+    fs.writeFileSync(fileName)
+}else{
+    console.log("Invalid Action")
+}
