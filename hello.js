b2397130f3e90dba0e62b2510707a89e761a4857
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-01-01 01:13:17 +0500

    hello world  node

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
+console.log("hello")
+
 // const math = require("./math")
 // console.log(math.add(2,3))
 // console.log(math.sub(2,3))
-const {add,sub} = require("./math")
-console.log(sub(2,3))
+// const {add,sub} = require("./math")
+// console.log(sub(2,3))
--- math.js
+++ math.js
@@ -1,11 +1,15 @@
-function add(a,b){
-    return a + b ;
+// function add(a,b){
+//     return a + b ;
 
-}
-function sub(a,b){
-    return a - b ;
+// }
+// function sub(a,b){
+//     return a - b ;
 
-}
+// }
+
+
+exports.add = (a,b)=> a +b
+
 module.exports  =  {
     add,sub
 }
