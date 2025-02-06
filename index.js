a639536c882726d19dc288e9aecf6cff7e5e80b2
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-02-06 19:28:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,23 @@ const userSchema = new mongoose.Schema({
   gender :{
     type :String,
   },
-})
+}) 
 mongoose.connect('mongodb+srv://iqra44938:vWAOuchONXPhdaHT@cluster0.mxfbz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connectejhjkwehd!'))
   .catch((err)=>console.log('error',err))
   // mongoose.connect('mongodb+srv://iqra44938:0h70s8xmdVd6G2J4@cluster0.yarkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   const User = mongoose.model('user',userSchema)
+
+  app.post('/api/users',async(req,res)=>{
+    const body = req.body 
+  const result =   await User.create({
+      firstName:body.firstName,
+      lastName:body.lastName,
+
+    })
+    console.log(result)
+  })
+  return res.status(201).json({msg:"success"})
 app.get("/",(req,res)=>{
    return res.send("Hello from home page")
 })
