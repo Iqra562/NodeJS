ac65783afe845e3b9eb3bd7807c1ba8324449a2e
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-02-27 05:35:56 -0800

    server creation

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const http = require('http');
+const fs = require('fs')
 const myServer  = http.createServer((req,res)=>{
-console.log("Request accepted")
-res.end("Hello from server ")
+    const log = `${Date.now()} : ${req.url} New req rec \n`
+    fs.appendFile('log.txt',log,(err,data)=>{
+        switch(req.url){
+            case '/' :  res.end('this is home page');
+            break;
+            case '/about':   res.end('this is about page');
+            break;
+            default :res.end("404")
+     }
+    })
 })
 
 myServer.listen(8000,()=>console.log("my server is running")) 
