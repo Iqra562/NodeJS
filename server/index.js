c4f9474edc7c5dc9af502fedc4913c8673f34853
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-02-28 04:07:52 -0800

    update

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const url = require('url');
 const myServer  = http.createServer((req,res)=>{
-    const log = `${Date.now()} : ${req.url} New req rec \n`
+    if (req.url === "/favicon.ico") return res.end()
+    const log = `${Date.now()} : ${req.url} New req rec \n`;
+    const myURL = url.parse(req.url,true)
+    console.log(myURL)
     fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
+        switch(myURL.pathname){
             case '/' :  res.end('this is home page');
             break;
-            case '/about':   res.end('this is about page');
+            case '/about': 
+            const urlName = myURL.query.name
+            res.end(`Hi ${urlName} `);
             break;
             default :res.end("404")
-     }
+     } 
     })
 })
 
